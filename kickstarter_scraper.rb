e7b2805638e2f57f13bc2499e7ed890fbf0bbd56
Remmy Clay <remmy.lauren@gmail.com>
2017-12-23 21:47:36 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-  
+
   kickstarter.css("li.project.grid_4").each do |project|
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
